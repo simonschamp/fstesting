378f8d601fda77f18c007f9fa8eb0cf2d5afb2c9
Simon Ihegbu <simon.ihegbu@student.lut.fi>
2026-05-17 00:13:40 +0300

    Modified the about and home pages

--- client/src/components/About.tsx
+++ client/src/components/About.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 const About = () => {
   return (
     <div>
@@ -11,6 +12,27 @@ const About = () => {
         Language Models (LLMs). Among many things the testing tools are expected
         to run different functionalities and then deliver metrics and reports
         which help us determine the reliability of this web application.
+      </p>
+      <p>
+        PLease register or log in with simple username and password. The app
+        works better when you register and log in.
+      </p>
+
+      <p>
+        This application is built with many features/components with integrated
+        microservices. The functions include: login, register functionalities.
+        Others include, authentication, drag-n-drop, translations with
+        integrated open API for fetching posts and comments from public data,
+        useEffect functions. The database is connectted and running. With a
+        React frontend and NodeJS/Express backend. This is a demo app with
+        minimal curated frontend. <br />
+        You can test the drag-and-drop feature in the "GETFORMINPUT" after
+        adding several messages!
+      </p>
+
+      <p>
+        Your feedback is welcome, you can contact me through:
+        simonsheg@outlook.com
       </p>
     </div>
   );
--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -2,10 +2,6 @@ const Home = () => {
   return (
     <div>
       <h1 data-cy="app-title">Welcome to Poems</h1>
-      <h3>
-        PLease register or log in with simple user and password. The app works
-        better when you register and log in.
-      </h3>
     </div>
   );
 };
